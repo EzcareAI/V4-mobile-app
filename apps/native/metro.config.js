75f0f33119df547a954655fe712cd7d3e1faab07
Joe Munene <joemunene984@gmail.com>
2026-02-07 02:52:59 +0300

    fix: Restore Uniwind config after NativeWind test

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -4,8 +4,6 @@ const { withUniwindConfig } = require("uniwind/metro");
 const config = getDefaultConfig(__dirname);
 
 config.resolver.unstable_enablePackageExports = true;
-
-// Disable React Compiler for web to avoid module loading issues
 config.resolver.disableHierarchicalLookup = false;
 
 module.exports = withUniwindConfig(config, {

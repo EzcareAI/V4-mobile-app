13d1884a312417960f3e98bb2326456127fa7608
Joe Munene <joemunene984@gmail.com>
2026-02-07 01:51:13 +0300

    fix: Re-enable Uniwind styling configuration

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -8,10 +8,8 @@ config.resolver.unstable_enablePackageExports = true;
 // Disable React Compiler for web to avoid module loading issues
 config.resolver.disableHierarchicalLookup = false;
 
-// module.exports = withUniwindConfig(config, {
-// 	cssEntryFile: "./global.css",
-// 	dtsFile: "./uniwind-types.d.ts",
-// 	extraThemes: ["light", "dark"],
-// });
-
-module.exports = config;
+module.exports = withUniwindConfig(config, {
+    cssEntryFile: "./global.css",
+    dtsFile: "./uniwind-types.d.ts",
+    extraThemes: ["light", "dark"],
+});

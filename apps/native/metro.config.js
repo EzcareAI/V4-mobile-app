61b042adfbef0a6a1a14a37fb4c8bff5969b6c93
Joe Munene <joemunene984@gmail.com>
2026-02-07 10:55:45 +0300

    fix: Limit Metro workers to 2 for EAS stability

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -6,6 +6,9 @@ const config = getDefaultConfig(__dirname);
 config.resolver.unstable_enablePackageExports = true;
 config.resolver.disableHierarchicalLookup = false;
 
+// Limit workers to prevent memory exhaustion on EAS Cloud
+config.maxWorkers = 2;
+
 module.exports = withUniwindConfig(config, {
     cssEntryFile: "./global.css",
     dtsFile: "./uniwind-types.d.ts",
